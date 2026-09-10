78934926fa1e91c19afed9d650288fa566043d73
Ayanami-Makima <2769514587@qq.com>
2026-09-10 15:46:37 +0800

    fix(moe): register shared expert metadata

--- ultralytics/mixture_metadata.py
+++ ultralytics/mixture_metadata.py
@@ -27,6 +27,7 @@ MIXTURE_MODULE_KINDS = MappingProxyType(
         "MultiHeadRouterMoE": "moe",
         "OptimalHybridGateMoE": "moe",
         "RefinedLowRankHybridAdaptiveGateMoE": "moe",
+        "SharedExpertMoE": "moe",
         "UltimateOptimizedMoE": "moe",
         "UltraOptimizedMoE": "moe",
         "VisualEnhancedAdaptiveGateMoE": "moe",
